f47f9ef9207dd0886d0ed1eac87ab14c6a80ef2a
dszamosi <daniel.szamosi@budapestkozut.hu>
2021-08-30 16:51:56 +0200

    initial commit

---
+++ site.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', () => {
+        // Toggle navigation
+        nav.classList.toggle('nav-active');
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+            }
+        });
+        // Burger animation
+        burger.classList.toggle('toggle');
+    });
+};
+
+navSlide();
